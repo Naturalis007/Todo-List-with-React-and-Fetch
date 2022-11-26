a8f4f0f0a580f871188150a94f8f4e151cca45be
Naturalis007 <tamara.salasvergara@gmail.com>
2022-11-26 16:40:19 +0000

    agregue api, y con useeffect la lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,123 @@
-import React from "react";
+// import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //create your first component
+// const Home = () => {
+// 	const [cosas, nueva] = useState (["Estar en la playa cuando hace frio", "Ver series de Netflix", "Tomar café de grano"]);
+// 	const [input, setInput] = useState("");
+// 	return (
+// 		<div className="text-center">
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+// 			<h1>Cosas que me hacen feliz:</h1>
+// 			<input placeholder="Escribe aquí" value={input} onChange={(event)=>{setInput(event.target.value) } } />
+// 			<button onClick={()=>{
+// 				nueva([...cosas, input])
+// 				setInput("")
+// 				}} >Agregar</button>
 
-export default Home;
+// 			{cosas.map((value,index,arr)=>{
+// 				return <li key={index}>{value} <button>X</button> </li>
+// 			})}
+
+// 		</div>
+// 	);
+// };
+
+// export default Home;
+
+import React, { useState } from "react";
+
+import { useEffect } from "react";
+
+/*
+export default function App() {
+  const [lista, setLista] = useState([]);
+
+//   function getGoT(){
+//     fetch("https://thronesapi.com/api/v2/Characters")
+//     	.then(response=>response.json())
+//     	.then(data=>console.log(data));
+//   }
+
+  useEffect(()=>{
+    fetch("https://pokeapi.co/api/v2/pokemon")
+    	.then(response=>response.json())
+    	.then(data=>setLista(data.results));
+
+  },[])
+
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+      {lista.map((value,index)=>{
+	  	return <li>{value.name}</li>
+	  })}
+    </div>
+  )
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>HOLA MUNDO!</h1>
+//     </div>
+//   );
+// }
+
+
+// export default App; 
+*/
+
+export default function App() {
+  const [lista, agregar] = useState([]);
+
+  function putApi() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(lista);
+
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://assets.breatheco.de/apis/fake/todos/user/naturalis",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }
+
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/naturalis")
+      .then((response) => response.json())
+      .then((data) => agregar(data));
+  }, []);
+
+  useEffect(()=>{
+	putApi();
+  }, [lista]);
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <form
+        onSubmit={(evento) => {
+          evento.preventDefault();
+		  agregar([...lista, {label:evento.target[0].value, done:false}]);
+	  }}
+      >
+        <input type="text" placeholder="agregar tarea" />
+      </form>
+      {lista.map((elm, index) => {
+        return <li key={index}>{elm.label} </li>;
+      })}
+      <p>Te falta {lista.length} tareas por terminar</p>
+    </div>
+  );
+}
